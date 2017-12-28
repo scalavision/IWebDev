98f1289d9de05780e92a4b4c33bb16439abde226
scalavision <scalavision@gmail.com>
2017-12-28 20:33:40 +0100

    removing unused files, adding comments to Init Interface

--- IClient/src/main/scala/iwebdev/client/api/Init.scala
+++ IClient/src/main/scala/iwebdev/client/api/Init.scala
@@ -3,6 +3,15 @@ package iwebdev.client.api
 import scala.scalajs.js
 import scala.scalajs.js.annotation.JSGlobal
 
+/**
+  * Global Interface for the Web client application to implement
+  *
+  * Whenever there is a new javascript installed into the dom, the run method will
+  * be called on the Web client application.
+  *
+  * TODO: enhance with other things like initial data setup, caching the url in use etc.
+  */
+
 @js.native
 @JSGlobal
 object Init extends js.Object {
